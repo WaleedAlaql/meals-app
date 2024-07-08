88804b91f11c235356f7d0043c2c5ad2fe189d1f
Waleed Alaql <waleedalaql10@gmail.com>
2024-07-08 15:51:04 +0300

    Adding Navigation to the MealDetails Screen

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/models/category.dart';
 import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
-  void selectCategory(BuildContext context) {
+  void selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
     Navigator.push(
-        context,
-        MaterialPageRoute(
-            builder: (ctx) => const MealsScreen(
-                title: 'Some title',
-                meals: []))); // Navigator.of(context).push(route) is alternative
+      context,
+      MaterialPageRoute(
+        builder: (ctx) => MealsScreen(
+          title: category.title,
+          meals: filteredMeals,
+        ),
+      ),
+    ); // Navigator.push(context, route) is alternative to Navigator.of(context).push(route)
   }
 
   @override
@@ -34,7 +41,7 @@ class CategoriesScreen extends StatelessWidget {
             CategoryGridItem(
               category: category,
               onSelectCategory: () {
-                selectCategory(context);
+                selectCategory(context, category);
               },
             )
         ],
--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/screens/meal_details.dart';
+import 'package:meals_app/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({super.key, required this.title, required this.meals});
 
   final String title;
   final List<Meal> meals;
+
+  void selectMeal(BuildContext context, Meal meal) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (ctx) => MealDetailsScreen(meal: meal),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,7 +46,8 @@ class MealsScreen extends StatelessWidget {
     if (meals.isNotEmpty) {
       content = ListView.builder(
         itemCount: meals.length,
-        itemBuilder: (ctx, index) => Text(meals[index].title),
+        itemBuilder: (ctx, index) =>
+            MealItem(meal: meals[index], onSelectMeal: selectMeal),
       );
     }
 
